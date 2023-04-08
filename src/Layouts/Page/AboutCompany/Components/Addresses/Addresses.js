4caf593c43ae0533b6a388416b7ae9739b29acf5
kategaleeva <129985311+kategaleeva@users.noreply.github.com>
2023-04-08 16:22:43 +0000

    кнопка добавить филиал вниз

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,13 +3,13 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className="col d-flex flex-column gap-2 mt-4 border-adresses p-4">
-            <div className='d-inline-flex flex-column gap-3'>
+        <div className="col d-flex flex-column justify-content-between gap-2 mt-4 border-adresses">
+            <div className='d-inline-flex flex-column gap-3 p-4'>
                 <div className='title'>Адреса заведений</div>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
-            <div><SecondaryButton>Добавить филиал</SecondaryButton></div>    
             </div>
+            <SecondaryButton>Добавить филиал</SecondaryButton>
         </div>
     );
 }
