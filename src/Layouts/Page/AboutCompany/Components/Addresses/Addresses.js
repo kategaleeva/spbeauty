7970c42a96b0dacdea2cb00b96d3fc9083313836
kategaleeva <129985311+kategaleeva@users.noreply.github.com>
2023-04-08 10:16:47 +0000

    изменила кнопку закрыть запись

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -4,8 +4,8 @@ import ListElement from "./ListElement";
 function Addresses() {
     return (
         <div className="col">
-            <div className='title'>Адреса заведений</div>
             <div className='d-inline-flex flex-column gap-3'>
+            <div className='title'>Адреса заведений</div>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 <SecondaryButton>Добавить филиал</SecondaryButton>
