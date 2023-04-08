dfe489fbfd53f1ba57fbe9da906f27a67b291da5
kategaleeva <129985311+kategaleeva@users.noreply.github.com>
2023-04-08 07:40:41 +0000

    добавила иконки вместо кнопок

--- src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/ListElement.js
@@ -3,6 +3,7 @@ import SecondaryButton from '../../../../../Components/SecondaryButton';
 import EditIcon from '../../../../../Icons/EditIcon';
 import RemoveIcon from '../../../../../Icons/RemoveIcon';
 import IconOnlyButton from '../../../../../Components/IconOnlyButton.js';
+import SettingsIcon from '../../../../../Icons/SettingsIcon';
 
 const Option = styled.div`
     outline: none;
@@ -16,8 +17,8 @@ function ListElement(props) {
     return (
         <div className='d-flex flex-row align-items-center align-self-stretch gap-2'>
             <Option className='flex-grow-1 p-2'>{props.children}</Option>
-            <SecondaryButton minWidth='0'>Ред.</SecondaryButton>
-            <SecondaryButton minWidth='0'>Удл.</SecondaryButton>
+            <IconOnlyButton icon={<EditIcon/>}/>
+            <IconOnlyButton icon={<RemoveIcon/>}/>
         </div>
     );
 }
