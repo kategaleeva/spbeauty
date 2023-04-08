140b051dcce6646875463ad567ae0ed3f2dce547
kategaleeva <129985311+kategaleeva@users.noreply.github.com>
2023-04-08 11:08:46 +0000

    адреса на одной вертикали с заголовком

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -4,6 +4,7 @@ import ListElement from "./ListElement";
 function Addresses() {
     return (
         <div className="col">
+            <div class="gap-4"></div>
             <div className='d-inline-flex flex-column gap-3'>
             <div className='title'>Адреса заведений</div>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
