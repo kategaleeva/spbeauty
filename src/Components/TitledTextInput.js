1f254184fe48c355db18f5d41f4ffd9fb97cd56a
kategaleeva <129985311+kategaleeva@users.noreply.github.com>
2023-04-08 06:26:16 +0000

    обновлены инпуты

--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -1,10 +1,18 @@
 import styled from "styled-components";
 
 const Input = styled.input`
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 8px 16px;
+gap: 16px;
+
+width: 447px;
+height: 34px;
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
+    border: 1px solid #E2E8F0;
+    border-radius: 24px;
+    background-color: #F8FAFC;
     color: #666787;
     &:hover {
         background-color: #D9E2EFB8;
@@ -16,9 +24,9 @@ function TitledTextInput({title, def}) {
         (<div></div>) : 
         (<div>{title}</div>);
     return (
-        <div className='d-flex flex-column gap-2'>
+        <div className='d-flex flex-column gap-1'>
             {Title}
-            <Input className='p-2' defaultValue={def} ></Input>
+            <Input className='p-3' defaultValue={def} ></Input>
         </div>
     );
 }
