8ca3d99d52198a4e680740b3164889922ba6ac21
kategaleeva <129985311+kategaleeva@users.noreply.github.com>
2023-04-08 04:41:08 +0000

    обонвили фон ховера

--- src/Layouts/Page/Header/Components/SearchField.js
+++ src/Layouts/Page/Header/Components/SearchField.js
@@ -15,7 +15,7 @@ const Input = styled.input`
     color: #152842;
     width: 100%;
     &:hover {
-        background: linear-gradient(180deg, #DFEAF1 0%, #CFDDE8 100%);
+        background: #F8FAFC;
     }
     &:focus {
         background: white;
@@ -26,7 +26,7 @@ const SearchIconWrapper = styled.div`
     background-color: #F8FAFC;
     position: absolute;
     box-sizing:border-box;
-    right: 0px;  /* Здесь можно добавить отступ от правого края, чтобы сдвинуть иконку левее */
+    right: 8px;  /* Здесь можно добавить отступ от правого края, чтобы сдвинуть иконку левее */
     top:50%;
     transform: translateY(-50%);
 `;
@@ -34,8 +34,8 @@ const SearchIconWrapper = styled.div`
 function SearchField() {
     return (
         <WrapperInput className='d-inline-flex'>  {/* Устанавливает ширину поля и включает относительное положение */}
-            <Input className='p-2' placeholder='Найти на странице...'/>
-            <SearchIconWrapper className='p-2'>  {/* Устанавливает абсолютное положение внутри поля ввода */}
+            <Input className='p-2' placeholder='Найти'/>
+            <SearchIconWrapper className='p-1'>  {/* Устанавливает абсолютное положение внутри поля ввода */}
                 <IconOnlyButton icon={<SearchIcon/>}/>
             </SearchIconWrapper>
         </WrapperInput>
