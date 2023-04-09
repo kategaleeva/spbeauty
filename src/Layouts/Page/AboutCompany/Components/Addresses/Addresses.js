0e1b49aebcd3da8f3a12f5b75b4f797961544c3f
kategaleeva <129985311+kategaleeva@users.noreply.github.com>
2023-04-09 04:37:22 +0000

    обновила бордер у адресов

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,7 +3,7 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-        <div className="col d-flex flex-column justify-content-between gap-2 mt-4 border-adresses">
+        <div className="col d-flex flex-column justify-content-between gap-2 mt-4 border-adresses me-5">
             <div className='d-inline-flex flex-column gap-3 p-4'>
                 <div className='title'>Адреса заведений</div>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
